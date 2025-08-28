16eea4dda388662207f24b09a6da2dc7f67faab1
Alina <alicia1997@mail.ru>
2025-08-28 16:20:15 +0300

    added

--- src/test/java/guru/qa/avitoTest.java
+++ src/test/java/guru/qa/avitoTest.java
@@ -3,7 +3,6 @@ package guru.qa;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
